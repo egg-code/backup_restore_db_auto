77bb6f0eebd6c5068bff285b4860d7e0bf1eab96
egg <kaungmyatkyaw.gg@gmail.com>
2025-04-23 22:28:09 +0700

    Modified restore.py

--- restore.py
+++ restore.py
@@ -8,10 +8,10 @@ from datetime import datetime
 logging.basicConfig(filename='./logs/restore.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 # Define Neon DB connection details
-DB_NAME = os.getenv('PGDATABASE')
-DB_USER = os.getenv('PGUSER')
-DB_PASSWORD = os.getenv('PGPASSWORD')
-DB_HOST = os.getenv('PGHOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
 DB_PORT = '5432'
 
 # Define backup directory
